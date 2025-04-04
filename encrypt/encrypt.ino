3fde919bf1d48c2767103dffeb29769f005a896a
Cesar Guillen Cuñat <91685879+cesar-guillen@users.noreply.github.com>
2025-04-04 19:58:09 +0200

    implemented simple decryption for chunked files
    To decrypt a chunk of cypher text we need to pass the length of the cypher text, since its not null terminated we need to tell the decryption function its length since we cannot compute it. Decrypting simple files produces the same hash but for larger ones it does not

--- encrypt/encrypt.ino
+++ encrypt/encrypt.ino
@@ -13,36 +13,35 @@
 #include "FS.h"
 #include "SD.h"
 #include "SPI.h"
-#define CHUNK_SIZE 2048
+#define CHUNK_SIZE 1
 
-int encrypt_chunk(char* plaintext, char* cyphertext, unsigned long long *clen){
+unsigned char npub[CRYPTO_NPUBBYTES] = {0}; //nonce
+unsigned char k[CRYPTO_KEYBYTES] = {0};     //key
+
+int encrypt_chunk(char* plaintext, char* cyphertext, unsigned long long *clen, size_t mlen){
   unsigned char *m = (unsigned char*)plaintext;
-  unsigned long long mlen = strlen((char*)m);
-
   unsigned char ad[] = "";
   unsigned long long adlen = strlen((char*)ad);
-
-  unsigned char npub[CRYPTO_NPUBBYTES] = {0}; //nonce
-  unsigned char k[CRYPTO_KEYBYTES] = {0};     //key
 
   int enc_success = crypto_aead_encrypt((unsigned char*)cyphertext, clen, m, mlen, ad, adlen, NULL, npub, k);
   return enc_success;
 }
-/*
-int decrypt_chunk(){
-  unsigned char decrypted_m[mlen + 1]; // decrypted plaintext buffer
-  unsigned long long decrypted_mlen;
-  //int decrypt_status = crypto_aead_decrypt(decrypted_m, &decrypted_mlen, NULL, c, clen, ad, adlen, npub, k);
 
+int decrypt_chunk(char *ciphertext,size_t clen ,char *plaintext, unsigned long long * decrypted_mlen){
+  unsigned char ad[] = "";  // Optional associated data
+  unsigned long long adlen = strlen((char*)ad);
+  int decrypt_status = crypto_aead_decrypt((unsigned char*)plaintext, decrypted_mlen, NULL, (const unsigned char*)ciphertext, clen, ad, adlen, npub, k);
   if (decrypt_status == 0) {
-      decrypted_m[decrypted_mlen] = '\0';
-      Serial.printf("Decrypted output: %s\n", decrypted_m);
+      plaintext[*decrypted_mlen] = '\0';
+      Serial.printf("Decrypted output: %s\n", plaintext);
   } else {
       Serial.println("Decryption failed!");
+      return -1;
   }
+  
   return 0;
 }
-*/
+
 void print_hex(const unsigned char* data, size_t len) {
   for (size_t i = 0; i < len; i++) {
     char buf[3];
@@ -51,7 +50,6 @@ void print_hex(const unsigned char* data, size_t len) {
   }
   Serial.println();
 }
-
 
 void encrypt_file(fs::FS &fs, const char *path){
   Serial.printf("Encrypting file contents of %s ... \n", path);
@@ -67,7 +65,7 @@ void encrypt_file(fs::FS &fs, const char *path){
   {
     size_t size_to_read = ((file_size - file_encrypted) > CHUNK_SIZE ? CHUNK_SIZE : (file_size - file_encrypted));
     char *curr_chunk = (char *)malloc(size_to_read);
-    char *encrypted_chunk = (char *)malloc(size_to_read * 2 + CRYPTO_ABYTES);
+    char *encrypted_chunk = (char *)malloc(size_to_read + CRYPTO_ABYTES);
     if ((curr_chunk == nullptr || encrypted_chunk == nullptr)) {
       Serial.println("Memory allocation failed");
       file.close();
@@ -75,7 +73,7 @@ void encrypt_file(fs::FS &fs, const char *path){
     }
     file.readBytes(curr_chunk, size_to_read);
     unsigned long long clen;
-    if (encrypt_chunk(curr_chunk, encrypted_chunk, &clen) < 0){
+    if (encrypt_chunk(curr_chunk, encrypted_chunk, &clen, size_to_read) < 0){
       Serial.printf("Encryption failed exiting...\n");
       free(curr_chunk);
       free(encrypted_chunk);
@@ -83,13 +81,18 @@ void encrypt_file(fs::FS &fs, const char *path){
       return;
     }
     //print_hex((unsigned char*)encrypted_chunk, clen);
+    unsigned long long decrypted_mlen;
+    char * plaintext = (char *)malloc(CHUNK_SIZE + 1);
+    decrypt_chunk(encrypted_chunk, clen, plaintext, &decrypted_mlen);
+    appendFile(fs, "/test2.txt", plaintext);
     file_encrypted += size_to_read;
     free(curr_chunk);
     free(encrypted_chunk);
+    free(plaintext);
   }
   unsigned long end_time = millis();
   unsigned long total_time = end_time - start_time;
-  Serial.printf("Encryption process completed in %lu seconds\n", (double)total_time / 1000.0);
+  Serial.printf("Encryption process completed in %.5f seconds\n", ((float)total_time) / 1000.0);
   file.close();
   return;
 }
@@ -108,7 +111,12 @@ void setup() {
   }
   listDir(SD, "/", 0);
   readFile(SD, "/test.txt");
-  encrypt_file(SD, "/zip.zip");
+  deleteFile(SD, "/test2.txt");
+  readFile(SD, "/test2.txt"); 
+  encrypt_file(SD, "/test.txt");
+  readFile(SD, "/test2.txt");
+  hash_file(SD, "/test.txt");
+  hash_file(SD, "/test2.txt");
 }
 
 void loop() {
