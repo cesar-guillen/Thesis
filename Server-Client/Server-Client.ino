e2020572176d49f24fa7259c25cf6b45629e5adc
Cesar Guillen Cuñat <91685879+cesar-guillen@users.noreply.github.com>
2025-04-10 14:56:09 +0200

    added sending hashes and encrypted chunks (later not working)

--- Server-Client/Server-Client.ino
+++ Server-Client/Server-Client.ino
@@ -10,23 +10,30 @@
 #include "../ascon-suite-master/apps/asconcrypt/readpass.c"
 #include "../ascon-suite-master/apps/asconcrypt/fileops.c"
 #include "../ascon-suite-master/apps/asconcrypt/asconcrypt.c"
+
 extern "C" {
   #include "esp_task_wdt.h"
 }
 
-const char* ssid = "esp";
-const char* password = "";
-
 // Server config
+const char* ssid = "esp";
+const char* password = "MyWifiZone123!";
 WiFiServer server(5000);
-
+#define CHUNK_SIZE 1024
 #define CRYPTO_BYTES 64
-#define METADATA_HASH (sizeof(size_t) + 1 + CRYPTO_BYTES)
-#define METADATA (sizeof(size_t) + 1) // This includes the size of the cyphertext chunk and a bit singaling if the hash should be read
+WiFiClient persistentClient;
 
 //const char* remoteIP = "192.168.244.201"; // red
 const char* remoteIP = "192.168.244.196"; // white
 const uint16_t remotePort = 5000;
+
+// global variables
+char unsigned  hash[CRYPTO_BYTES] = { 0 };
+String requested_file_name = "";
+const uint8_t hash_code = 1;
+const uint8_t data_code = 0;
+const uint8_t msg_code = 47;
+
 
 // Task handles
 TaskHandle_t serverTaskHandle = NULL;
@@ -46,33 +53,77 @@ String get_file_name(String msg) {
   return "/" + secondWord;
 }
 
-void send_file(String file_name){
+int check_file(fs::FS &fs, const char *path){
+  File file = fs.open(path);
+  if (!file) {
+    Serial.println("File does not exist");
+    return -1;
+  }
+  file.close();
+  return 0;
+}
+
+void send_hash(fs::FS &fs, const char *original_file) {
+  unsigned char hash[CRYPTO_BYTES] = { 0 };
+  hash_file(fs, original_file, hash);
+  print_hash_output(4, hash);
+  size_t payload_size =  CRYPTO_BYTES + sizeof(hash_code);
+  uint8_t *payload = (uint8_t *)malloc(payload_size);
+  if (payload == nullptr) {
+    Serial.println("Memory allocation failed");
+    return;
+  }
+  memcpy(payload, &hash_code, sizeof(hash_code));
+  memcpy(payload + sizeof(hash_code), hash, CRYPTO_BYTES);
+  persistentClient.write(payload, payload_size);
+  free(payload);
+}
+
+void send_file(fs::FS &fs, const char *encrypted_file, const char* original_file){
+  File file = fs.open(encrypted_file);
+  if (!file) {
+    Serial.println("Failed to open file");
+    return;
+  }
+  size_t file_size = file.size();
+  size_t file_sent = 0;
+  while (file_sent < file_size)
+  {
+    size_t size_to_send = ((file_size - file_sent) > CHUNK_SIZE ? CHUNK_SIZE : (file_size - file_sent));
+    char *curr_chunk = (char *)malloc(size_to_send + sizeof(data_code) + sizeof(size_to_send));
+    if (curr_chunk == nullptr) {
+      Serial.println("Memory allocation failed");
+      file.close();
+      return;
+    }
+    memcpy(curr_chunk, &data_code, sizeof(data_code));
+    memcpy(curr_chunk + sizeof(data_code), &size_to_send, sizeof(size_to_send));
+    file.readBytes(curr_chunk + sizeof(size_to_send) + sizeof(data_code), size_to_send);
+    persistentClient.write((uint8_t*)curr_chunk, size_to_send + sizeof(size_to_send) + sizeof(data_code));
+    file_sent += size_to_send;
+    free(curr_chunk);
+  }
+  file.close();
+  return;
+}
+
+void prepare_file(String file_name){
   if (!SD.begin()) {
     Serial.println("Card Mount Failed");
     return;
   }
-
-  char unsigned finalhash[CRYPTO_BYTES] = { 0 };
-  char unsigned compare[CRYPTO_BYTES] = { 0 };
-
+  if (check_file(SD,file_name.c_str()) < 0) {
+    return;
+  }
   String encrypted_filed_name = file_name + ".ascon";
-  Serial.printf("Encrypting file: %s\n", file_name);
+  Serial.printf("Encrypting file %s ...\n", file_name);
   encrypt_file(file_name.c_str(), &SD, encrypted_filed_name.c_str());
-  Serial.printf("Decrypting file: %s\n", encrypted_filed_name);
-  decrypt_file(encrypted_filed_name.c_str(), &SD, "/decrypted.txt");
-
-  hash_file(SD, "/decrypted.txt", compare);
-  hash_file(SD, file_name.c_str(), finalhash);
-  Serial.printf("Comparing hashes ...\n");
-  print_hash_output(4, finalhash);
-  print_hash_output(4, compare);
-
-  listDir(SD, "/", 1);
+  send_hash(SD, file_name.c_str());
+  send_file(SD, encrypted_filed_name.c_str(), file_name.c_str());
 }
 
-int parse_input(String msg) {
-  msg.trim();
-  if (msg.startsWith("/GET")) {
+int parse_request(String msg) {
+  if (msg.startsWith("GET")) {
     Serial.println("Command recognized: GET");
     String file_name = get_file_name(msg);
     if (file_name == "") {
@@ -80,15 +131,51 @@ int parse_input(String msg) {
       return -1;
     }
     Serial.printf("File name is: %s\n", file_name);
-    send_file(file_name);
+    prepare_file(file_name);
     return 0;
   }
-
-  Serial.println("Unrecognized command.");
   return -1;
 }
 
-// Server task
+void parse_input(WiFiClient& client) {
+  uint8_t msg_type = 2;
+  client.readBytes((char*)&msg_type, sizeof(msg_type));
+  if (msg_type == hash_code) {
+    Serial.println("hash received, reading it into hash...");
+    if (client.available() >= CRYPTO_BYTES) {
+      client.readBytes((char*)hash, CRYPTO_BYTES);
+      print_hash_output(4, hash);
+    }
+  }
+  else if (msg_type == data_code) {
+    if (!SD.begin()) {
+      Serial.println("Card Mount Failed");
+      return;
+    }
+    Serial.printf("DATA received, reading it into %s.ascon ...\n",  requested_file_name);
+    size_t size_to_read;
+    if (client.readBytes((char*)&size_to_read, sizeof(size_t)) != sizeof(size_t)) {
+      Serial.println("Failed to read chunk size");
+      return;
+    }
+    unsigned char* curr_chunk = (unsigned char*)malloc(size_to_read);
+    if (client.readBytes((char*)curr_chunk, size_to_read) != size_to_read) {
+      Serial.println("Failed to read full chunk");
+      free(curr_chunk);
+      return;
+    }
+    appendFile(SD, (requested_file_name+".ascon1").c_str(), (const char*)curr_chunk, size_to_read);
+    free(curr_chunk);
+  }
+  else if (msg_type == msg_code) {
+    String msg = client.readStringUntil('\n');
+    Serial.print("[Server] Received: ");
+    Serial.println(msg);
+    parse_request(msg);
+  }
+  Serial.printf("msg_type is : %u\n", msg_type);
+}
+
 void serverTask(void* parameter) {
   server.begin();
   Serial.println("[Server] Started on port 5000");
@@ -99,23 +186,17 @@ void serverTask(void* parameter) {
       Serial.println("[Server] Client connected");
       while (client.connected()) {
         if (client.available()) {
-          String msg = client.readStringUntil('\n');
-          Serial.print("[Server] Received: ");
-          Serial.println(msg);
-          parse_input(msg);
-        } else{
-          delay(1);
+          parse_input(client);
+        } else {
+          delay(1);  
         }
       }
       client.stop();
       Serial.println("[Server] Client disconnected");
     }
-    delay(10); // avoid watchdog timeout
+    delay(10); 
   }
 }
-
-
-WiFiClient persistentClient;
 
 void clientTask(void* parameter) {
   delay(5000);  // Wait for WiFi and server to start
@@ -135,11 +216,11 @@ void clientTask(void* parameter) {
     if (Serial.available()) {
       String input = Serial.readStringUntil('\n');
       input.trim(); // Remove newline/extra whitespace
-
       if (input.length() > 0) {
         Serial.print("[Client] Sending message: ");
         Serial.println(input);
         persistentClient.println(input); 
+        requested_file_name = get_file_name(input);
       }
     }
     delay(100); 
