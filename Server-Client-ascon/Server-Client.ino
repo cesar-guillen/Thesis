868c4b4d46841acfab77cc85b0e77f40abe1510d
cesar-guillen <cesar@PC1>
2025-04-17 09:13:55 +0200

    renaming folder

---
+++ Server-Client-ascon/Server-Client.ino
@@ -0,0 +1,151 @@
+#include <WiFi.h>
+#include <ASCON.h>
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+#include <stdio.h>
+#include <string.h>
+#include "../ascon-suite-master/apps/asconcrypt/fileops.h"
+#include "../ascon-suite-master/apps/asconcrypt/readpass.h"
+#include "../ascon-suite-master/apps/asconcrypt/readpass.c"
+#include "../ascon-suite-master/apps/asconcrypt/fileops.c"
+#include "../ascon-suite-master/apps/asconcrypt/asconcrypt.c"
+extern "C" {
+  #include "esp_task_wdt.h"
+}
+
+#define CRYPTO_BYTES 64   // Size of hash
+#define MAX_NONCES 2000
+
+// Server config
+const char* ssid = "esp";
+const char* password = "MyWifiZone123!";
+WiFiServer server(5000);
+WiFiClient persistentClient;
+//const char* remoteIP = "192.168.244.201"; // red
+const char* remoteIP = "192.168.244.196"; // white
+const uint16_t remotePort = 5000;
+
+// global variables
+char unsigned  hash[CRYPTO_BYTES] = { 0 };
+String requested_file_name = "";
+const uint8_t hash_code = 1;
+const uint8_t data_code = 0;
+const uint8_t msg_code = 47;
+size_t nonces_table[MAX_NONCES] = {0};
+unsigned char npub[ASCON128_NONCE_SIZE] = {0};
+unsigned char current_nonce[ASCON128_NONCE_SIZE] = {0};
+unsigned char k[ASCON128_KEY_SIZE] = {0};     //key
+
+// Task handles
+TaskHandle_t serverTaskHandle = NULL;
+TaskHandle_t clientTaskHandle = NULL;
+
+void print_nonce(const unsigned char* npub) {
+    for (int i = 0; i < ASCON128_NONCE_SIZE; i++) {
+        if (npub[i] < 0x10) Serial.print("0");
+        Serial.print(npub[i], HEX);
+    }
+    Serial.println();
+}
+
+size_t nonce_to_integer(const unsigned char* npub){
+  size_t nonce_value = 0;
+  for (int i = 0; i < sizeof(size_t); i++) {
+    nonce_value <<= 8;
+    nonce_value |= npub[ASCON128_NONCE_SIZE - sizeof(size_t) + i];
+  }
+  return nonce_value;
+}
+
+String get_file_name(String msg) {
+  msg.trim();
+  int firstSpace = msg.indexOf(' ');
+  if (firstSpace == -1) {
+    return "";
+  }
+  String secondWord = msg.substring(firstSpace + 1);
+  int secondSpace = secondWord.indexOf(' ');
+  if (secondSpace != -1) {
+    return secondWord.substring(0, secondSpace);
+  }
+  return "/" + secondWord;
+}
+
+int check_file(fs::FS &fs, const char *path){
+  File file = fs.open(path);
+  if (!file) {
+    Serial.println("File does not exist");
+    return -1;
+  }
+  file.close();
+  return 0;
+}
+
+
+void serverTask(void* parameter) {
+  server.begin();
+  Serial.println("[Server] Started on port 5000");
+
+  while (true) {
+    WiFiClient client = server.available();
+    if (client) {
+      Serial.println("[Server] Client connected");
+      while (client.connected()) {
+        if (client.available()) {
+          recieve_input(client);
+        } else {
+          delay(1);  
+        }
+      }
+      client.stop();
+      Serial.println("[Server] Client disconnected");
+    }
+    delay(10); 
+  }
+}
+
+void clientTask(void* parameter) {
+  delay(5000);  // Wait for WiFi and server to start
+
+  while (true) {
+    if (!persistentClient.connected()) {
+      Serial.println("[Client] Connecting to remote...");
+      if (persistentClient.connect(remoteIP, remotePort)) {
+        Serial.println("[Client] Connected.");
+      } else {
+        Serial.println("[Client] Failed to connect, retrying in 5 seconds...");
+        delay(5000);
+        continue;
+      }
+    }
+    if (Serial.available()) { // the following code reads the text from the terminal and sends it to the new client, it also appends the neccesary metadata.
+      String input = Serial.readStringUntil('\n');
+      input.trim(); 
+      send_request(input);
+    }
+    delay(100); 
+  }
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  esp_task_wdt_deinit(); // watchdog cries without this. It believes funcitons get stuck when they do a lot of computing.
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nWiFi connected. IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Start tasks
+  xTaskCreatePinnedToCore(serverTask,"Server Task",16384,NULL,1,&serverTaskHandle,1);
+  xTaskCreatePinnedToCore(clientTask,"Client Task",8192,NULL,1,&clientTaskHandle,0);
+}
+
+void loop() {
+}
---
+++ Server-Client-ascon/sending.ino
@@ -0,0 +1,115 @@
+#define CHUNK_SIZE 512
+#define MAX_ENCRYPTED_MSG_SIZE 100
+
+void send_request(String input) {
+  Serial.print("[Client] Sending message: ");
+  Serial.println(input);
+
+  char* msg = (char*)input.c_str();
+  size_t msg_len = strlen(msg);
+  char encrypted_msg[MAX_ENCRYPTED_MSG_SIZE];
+  size_t clen = 0;
+  encrypt_message(msg, encrypted_msg, &clen, msg_len, npub);
+  size_t total_payload_size = sizeof(msg_code) + ASCON128_NONCE_SIZE + clen;
+  size_t total_packet_size = sizeof(size_t) + total_payload_size;
+
+  char* buffer = (char*)malloc(total_packet_size);
+  if (!buffer) {
+    Serial.println("Failed to allocate memory for message");
+    return;
+  }
+
+  size_t offset = 0;
+  memcpy(buffer + offset, &total_payload_size, sizeof(total_payload_size)); offset += sizeof(total_payload_size);
+  memcpy(buffer + offset, &msg_code, sizeof(msg_code)); offset += sizeof(msg_code);
+  memcpy(buffer + offset, npub, ASCON128_NONCE_SIZE); offset += ASCON128_NONCE_SIZE;
+  memcpy(buffer + offset, encrypted_msg, clen); offset += clen;
+  persistentClient.write((uint8_t*)buffer, total_packet_size);
+  free(buffer);
+  ascon_aead_increment_nonce(npub);
+  requested_file_name = get_file_name(input);
+  if (!SD.begin()) {
+    Serial.println("Card Mount Failed");
+    return;
+  }
+  deleteFile(SD, (requested_file_name + ".ascon").c_str());
+}
+
+
+void send_hash(fs::FS &fs, const char *original_file) {
+  unsigned char hash[CRYPTO_BYTES] = { 0 };
+  hash_file(fs, original_file, hash);
+  print_hash_output(4, hash);
+  // Encrypt the hash
+  char encrypted_hash[MAX_ENCRYPTED_MSG_SIZE] = { 0 };
+  size_t clen = 0;
+  encrypt_message((char*)hash, encrypted_hash, &clen, CRYPTO_BYTES, npub);
+
+  size_t payload_size = sizeof(hash_code) + ASCON128_NONCE_SIZE + clen;
+  size_t total_size = sizeof(size_t) + payload_size;
+
+  uint8_t *payload = (uint8_t *)malloc(total_size);
+  if (payload == nullptr) {
+    Serial.println("Memory allocation failed");
+    return;
+  }
+
+  size_t offset = 0;
+  memcpy(payload + offset, &payload_size, sizeof(payload_size)); offset += sizeof(payload_size);
+  memcpy(payload + offset, &hash_code, sizeof(hash_code)); offset += sizeof(hash_code);
+  memcpy(payload + offset, npub, ASCON128_NONCE_SIZE); offset += ASCON128_NONCE_SIZE;
+  memcpy(payload + offset, encrypted_hash, clen); offset += clen;
+
+  persistentClient.write(payload, total_size);
+  free(payload);
+
+  ascon_aead_increment_nonce(npub);
+}
+
+
+
+void send_file(fs::FS &fs, const char *encrypted_file, const char* original_file) {
+  File file = fs.open(encrypted_file);
+  if (!file) {
+    Serial.println("Failed to open file");
+    return;
+  }
+
+  size_t file_size = file.size();
+  size_t file_sent = 0;
+  Serial.println("Sending file ...");
+  while (file_sent < file_size) {
+    size_t size_to_send = ((file_size - file_sent) > CHUNK_SIZE ? CHUNK_SIZE : (file_size - file_sent));
+    uint8_t last_chunk = (file_sent + size_to_send == file_size) ? 1 : 0;
+    size_t payload_size = sizeof(data_code) + sizeof(last_chunk) + sizeof(size_to_send) + size_to_send;
+    size_t total_message_size = sizeof(data_code) + sizeof(last_chunk) + sizeof(size_to_send) + size_to_send;
+    size_t total_packet_size = sizeof(size_t) + total_message_size;
+
+    char *buffer = (char *)malloc(total_packet_size);
+    if (!buffer) {
+      Serial.println("Memory allocation failed");
+      file.close();
+      return;
+    }
+
+    size_t offset = 0;
+    memcpy(buffer + offset, &total_message_size, sizeof(total_message_size)); offset += sizeof(total_message_size);
+    memcpy(buffer + offset, &data_code, sizeof(data_code)); offset += sizeof(data_code);
+    memcpy(buffer + offset, &last_chunk, sizeof(last_chunk)); offset += sizeof(last_chunk);
+    memcpy(buffer + offset, &size_to_send, sizeof(size_to_send)); offset += sizeof(size_to_send);
+    file.readBytes(buffer + offset, size_to_send);
+    int bytes_written = persistentClient.write((uint8_t*)buffer, total_packet_size);
+
+    if (bytes_written != total_packet_size) {
+      Serial.printf("Failed to write full chunk: wrote %d of %d bytes\n", bytes_written, total_packet_size);
+    }
+    delay(200); //delay to not overwhelm the client
+    file_sent += size_to_send;
+    float percent = ((float)file_sent / (float)file_size) * 100.0f;
+    Serial.printf("Progress: %.2f%%\n", percent);
+    free(buffer);
+  }
+
+  file.close();
+}
+
